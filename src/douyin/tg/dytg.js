eeb5c612373d37c0eb2359c313a09dd65078d6ff
fanzq <fzq1057600474@gamil.com>
2025-09-07 20:33:56 +0800

    finish
    1
    
    1
    
    1
    
    1
    
    1

--- src/douyin/tg/dytg.js
+++ src/douyin/tg/dytg.js
@@ -7,6 +7,9 @@ const withdrawUrl =
 const queryOrderUrl =
   'https://open.douyin.com/goodlife/v1/settle/bill/catering_query/';
 
+const queryLegerUrl =
+  'https://open.douyin.com/goodlife/v1/settle/bill/query_leger_url/';
+
 // 抖音token
 const access_token = '';
 
@@ -19,7 +22,14 @@ const DEFAULT_END_DATE = dayjs().format('YYYY-MM-DD'); // 今天
 
 const DEFAULT_SIZE = 50; // 每页数量，最大50
 
-getCateringWithdrawInfo();
+// 主函数
+async function main() {
+  var res = await getBillFile();
+  console.log(res);
+}
+
+// 执行主函数
+main().catch(console.error);
 
 async function getCateringWithdrawInfo() {
   try {
@@ -34,7 +44,7 @@ async function getCateringWithdrawInfo() {
       'access-token': access_token,
     });
     console.log(JSON.stringify(res.data.bill_withdraw_records, null, 2));
-    return res;
+    return res.data.bill_withdraw_records;
   } catch (error) {
     console.error('获取餐饮提现信息失败:', error);
   }
@@ -56,5 +66,21 @@ async function getOrderByWithdrawId(withdrawId) {
     return res;
   } catch (error) {
     console.error('餐饮账单详情查询失败:', error);
+  }
+}
+
+async function getBillFile() {
+  try {
+    var params = {
+      account_id: ACCOUNT_ID,
+      launch_date: '2025-09-12',
+    };
+    const res = await getWithHeaders(queryLegerUrl, params, {
+      'access-token': access_token,
+    });
+    console.log(JSON.stringify(res.data, null, 2));
+    return res;
+  } catch (error) {
+    console.error('离线分账单查询失败:', error);
   }
 }
